e0e84df6b57b67101125b871045eca95ee0e24d2
thedignityofcoffee <thedignityofcoffee@outlook.com>
2025-05-06 15:20:30 +0800

    Change package name.

--- src/ZooManagementSystem/Animal.java
+++ src/ZooManagementSystem/Animal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package oop.demo.zoomanagementsystem;
+package ZooManagementSystem;
 
 /**
  * Animal Class - Represents animals in the zoo
