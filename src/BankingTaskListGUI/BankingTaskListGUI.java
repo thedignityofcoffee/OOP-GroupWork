eb4492fc0e0f7b38448feb82139ec0f17a2ad863
Jerry Lee <69750103+lijierui49468@users.noreply.github.com>
2025-05-17 14:11:42 +0800

    Change the initial size BankingTaskListGUI

--- src/BankingTaskListGUI/BankingTaskListGUI.java
+++ src/BankingTaskListGUI/BankingTaskListGUI.java
@@ -54,7 +54,7 @@ public class BankingTaskListGUI {
     private void initializeFrame() {
         frame = new JFrame("Banking Task Management System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1000, 420);
         frame.setLayout(new BorderLayout(10, 10));
 
         // Create main panels
